06856c16b8b6ac8909353eb89ce1430bee7e7d4b
Azef1 <azef@yopmail.com>
2016-06-20 16:23:16 +0200

    Check Hive: Send SQL query

---
+++ check_hive_databases.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Author: Adrian PORTE
+#Script to send SHOW DATABASES to HiveServer2 
+#$1 = HOST
+#$2 = PORT
+#$3 = REALMNAME
+#Example ./check_hive_databases.sh noeyy3fn.noe.edf.fr 10001 HADOOP_DEV.EDF.FR
+
+beeline -u "jdbc:hive2://$1:$2/;principal=hive/_HOST@$3;transportMode=http;httpPath=cliservice;ssl=true" --silent=true --outputformat=csv -f /tmp/nagios/command.txt --showWarnings=false 
+
+
+if  [ $? -eq 0 ]; then
+	echo "OK - Query executed"
+	exit 0
+else
+	echo "CRITICAL - Query not executed"
+	exit 2
+fi
+
+
+
