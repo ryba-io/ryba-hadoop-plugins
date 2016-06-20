13345140fc0908299b6dd623a3a6291b2fd49683
Azef1 <azef@yopmail.com>
2016-06-20 16:24:37 +0200

    Update check_hive_databases.sh

--- check_hive_databases.sh
+++ check_hive_databases.sh
@@ -6,7 +6,7 @@
 #$3 = REALMNAME
 #Example ./check_hive_databases.sh noeyy3fn.noe.edf.fr 10001 HADOOP_DEV.EDF.FR
 
-beeline -u "jdbc:hive2://$1:$2/;principal=hive/_HOST@$3;transportMode=http;httpPath=cliservice;ssl=true" --silent=true --outputformat=csv -f /tmp/nagios/command.txt --showWarnings=false 
+beeline -u "jdbc:hive2://$1:$2/;principal=hive/_HOST@$3;transportMode=http;httpPath=cliservice;ssl=true" --silent=true --outputformat=csv -f command.txt --showWarnings=false 
 
 
 if  [ $? -eq 0 ]; then
