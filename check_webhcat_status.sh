c3e788501215da4664e7eab3dc7c44e8e02fa440
Azef1 <azef@yopmail.com>
2016-06-23 10:47:22 +0200

    Create check_webhcat_status.sh
    Curl API WebHCat status

---
+++ check_webhcat_status.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Author: Adrian PORTE
+#Usage ./check_webhcat_status.sh HOST PORT
+
+regex="^.*\"status\":\"ok\".*<status_code:200>$"
+out=`curl --negotiate -u : -s -w '<status_code:%{http_code}>' http://$1:$2/templeton/v1/status 2>&1`
+if [[ $out =~ $regex ]]; then
+  echo "OK: WebHCat Server status [$out]";
+  exit 0;
+fi
+echo "CRITICAL: Error accessing WebHCat Server, status [$out]";
+exit 2;
