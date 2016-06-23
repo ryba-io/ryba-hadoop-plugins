8014ef32105220e96893a88f7a2706b7fca86713
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2016-06-23 20:12:06 +0200

    fix check_hive_databases

--- check_hive_databases.sh
+++ check_hive_databases.sh
@@ -1,18 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Author: Adrian PORTE
-#Script to send SHOW DATABASES to HiveServer2 
+#Script to send SHOW DATABASES to HiveServer2
 #$1 = HOST
 #$2 = PORT
 #$3 = REALMNAME
+#$4 = USER
+#$5 = PASSWD
 #Example ./check_hive_databases.sh noeyy3fn.noe.edf.fr 10001 HADOOP_DEV.EDF.FR
 
-beeline -u "jdbc:hive2://$1:$2/;principal=hive/_HOST@$3;transportMode=http;httpPath=cliservice;ssl=true" --silent=true --outputformat=csv -f command.txt --showWarnings=false 
+#echo "HOST: $1"
+#echo "PORT: $2"
+#echo "REALM: $3"
+#echo "USER: $4"
 
+echo $5 | kinit $4@$3 >/dev/null 2>&1;
+
+#klist;
+
+beeline -u "jdbc:hive2://$1:$2/;principal=hive/_HOST@$3;transportMode=http;httpPath=cliservice;ssl=true" --silent=true --outputformat=csv -e "show databases;" --showWarnings=false >/dev/null 2>&1;
 
 if  [ $? -eq 0 ]; then
-	echo "OK - Query executed"
-	exit 0
+  echo "OK - Query executed"
+  exit 0
 else
-	echo "CRITICAL - Query not executed"
-	exit 2
+  echo "CRITICAL - Query not executed"
+  exit 2
 fi
