ec90d5bfae6a7ba0b1d6c3831306ce78ba3b1676
Azef1 <azef@yopmail.com>
2016-06-23 14:54:13 +0200

    Improve output text

--- check_webhcat_database.sh
+++ check_webhcat_database.sh
@@ -6,8 +6,8 @@ regex="^.*\"tables\".*<status_code:200>$"
 out=`curl --negotiate -u : -s -w '<status_code:%{http_code}>' -m 20 http://$1:$2/templeton/v1/ddl/database/default/table/?user.name=hive 2>&1`
 
 if [[ $out =~ $regex ]]; then
-  echo "OK: WebHCat Server database";
+  echo "OK: WebHCat Server database [${out:0:30}...]";
   exit 0;
 fi
-echo "CRITICAL: Error accessing WebHCat Server";
+echo "CRITICAL: Error accessing WebHCat Server [${out:0:30}...]";
 exit 2;
