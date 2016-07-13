c7b0c2baeca67504254c5569ba30f2f1de987564
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2016-07-13 15:10:59 +0200

    add ssh service wrapper

---
+++ ssh_service.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# ./ $SERVICESTATE$ $SERVICEATTEMPTS$
+
+HOST=$1
+STATE=$2
+ATTEMPT=$3
+ACTION=$4
+SERVICE=$5
+
+if [ "$STATE" != "CRITICAL" ]; then
+  echo "Not critical. Nothing to do";
+  exit 0;
+elif [ "$ATTEMPT" != "2" ]; then
+  echo "waiting for 2nd attempt. Nothing to do";
+  exit 0;
+fi
+
+ssh -o "StrictHostkeyChecking no" root@${HOST} service ${SERVICE} ${ACTION}
